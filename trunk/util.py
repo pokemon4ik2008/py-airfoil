20cced6b0f6388742be7c62dc05b3d3992601723
paul.features@gmail.com <paul.features@gmail.com@60fda64a-5d68-a757-9b40-cfa5bbaf896f>
2011-03-09 21:02:46 +0000

    Added the ability for air resistance to cause the plane to rotate. Stall turns ALMOST possible!

--- trunk/util.py
+++ trunk/util.py
@@ -5,3 +5,8 @@ def limitToMaxValue(input, max):
         return -max
     else:
         return input
+
+global prettyfloat
+class prettyfloat(float):
+    def __repr__(self):
+        return "%0.2f" % self
