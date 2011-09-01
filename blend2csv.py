ae5ba2793c18a1ab3e6172c6eb8d5487f23860d7
K1eran.Wh17e@gmail.com <K1eran.Wh17e@gmail.com@60fda64a-5d68-a757-9b40-cfa5bbaf896f>
2011-09-01 11:26:05 +0000

    blender export fixes

--- blend2csv.py
+++ blend2csv.py
@@ -6,12 +6,15 @@ from mathutils import Color
 print('Objects, ', len(bpy.data.meshes))
 print('Object:, First Vertice:, Vertices:, First Triangle:, Triangles:')
 
+obs=bpy.context.scene.objects
+
 v_tot=0
-f_tot=0
+t_tot=0
 v_next=1
 t_next=1
 tri2vert={}
-for m in bpy.data.meshes:
+for o in obs:
+    m=o.data
     print(m.name, ', ', v_next, ', ', len(m.vertices), ', ', t_next, ', ', len(m.faces)*2)
     v_next+=len(m.vertices)
     t_next+=len(m.faces)*2
@@ -19,47 +22,61 @@ for m in bpy.data.meshes:
         v_tot+=1
 
     for f in m.faces:
-        f_tot+=1
+        verts=[]
+        for v_ref in f.vertices:
+            verts.append(v_ref)
+        try:
+            t_tot+=1
+            if len(verts)==4:
+                t_tot+=1
+            assert len(verts)<=4
+        except AssertionError:
+            print('Expecting 4 verts while counting: ', verts, ' m: ', m, ' f: ', f)
+            exit(-1)
 
 print()
 print('Vertices, ',v_tot)
 print('Vertice:, X:, Y:, Z:')
 v_num=0
-for m in bpy.data.meshes:
+for o in obs:
+    m=o.data
     for v in m.vertices:
         v_num+=1
         tri2vert[m,v.index]=v_num
-        print(v_num, v.co.x, v.co.y, v.co.z)
+        print(v_num,', ', v.co.x, ', ', v.co.y, ', ', v.co.z)
         #if m.name == 'Circle':
         #    print('lookup: m ', m, 'v ', v, 'v_num ', v_num)
 
 print()
-print('Triangles, ', f_tot*2)
+print('Triangles, ', t_tot)
 print('Triangle:, Side1:, Side2:, Side3:')
 
-f_num=0
-for m in bpy.data.meshes:
+t_num=0
+for o in obs:
+    m=o.data
     for f in m.faces:
         verts=[]
         for v_ref in f.vertices:
             verts.append(v_ref)
-        f_num+=1
         try:
-            print(f_num*2-1, ', ', tri2vert[m,verts[0]], ', ', tri2vert[m,verts[1]], ', ', tri2vert[m,verts[2]])
+            t_num+=1
+            print(t_num, ', ', tri2vert[m,verts[0]], ', ', tri2vert[m,verts[1]], ', ', tri2vert[m,verts[2]])
             if len(verts)==4:
-                print(f_num*2, ', ', tri2vert[m,verts[1]], ', ', tri2vert[m,verts[2]], ', ', tri2vert[m,verts[3]])
+                t_num+=1
+                print(t_num, ', ', tri2vert[m,verts[0]], ', ', tri2vert[m,verts[2]], ', ', tri2vert[m,verts[3]])
             assert len(verts)<=4
         except AssertionError:
-            print('Expecting 4 verts: ', verts, ' m: ', m, ' f: ', f)
+            print('Expecting 4 verts in triangles: ', verts, ' m: ', m, ' f: ', f)
             exit(-1)
 
 print()
-print('Colors, ', f_num*2)
+print('Colors, ', t_tot)
 print('Triangle:, Red:, Green:, Blue:')
 
-f_num=0
+t_num=0
 BLACK=Color((0,0,0))
-for m in bpy.data.meshes:
+for o in obs:
+    m=o.data
     if len(m.materials):
         c=m.materials[0].diffuse_color
     else:
@@ -68,6 +85,13 @@ for m in bpy.data.meshes:
         verts=[]
         for v_ref in f.vertices:
             verts.append(v_ref)
-        f_num+=1
-        print(f_num*2-1, ', ', round(c.r*0xff), ', ', round(c.g*0xff), ', ', round(c.b*0xff))
-        print(f_num*2, ', ', round(c.r*0xff), ', ', round(c.g*0xff), ', ', round(c.b*0xff))
+        try:
+            t_num+=1
+            print(t_num, ', ', round(c.r*0xff), ', ', round(c.g*0xff), ', ', round(c.b*0xff))
+            if len(verts)==4:
+                t_num+=1
+                print(t_num, ', ', round(c.r*0xff), ', ', round(c.g*0xff), ', ', round(c.b*0xff))
+            assert len(verts)<=4
+        except AssertionError:
+            print('Expecting 4 verts in colours: ', verts, ' m: ', m, ' f: ', f)
+            exit(-1)
