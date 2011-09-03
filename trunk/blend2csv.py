f084e92e5c49cb0d1d4268318e57ee2ecb3b3b42
K1eran.Wh17e@gmail.com <K1eran.Wh17e@gmail.com@60fda64a-5d68-a757-9b40-cfa5bbaf896f>
2011-09-03 20:58:02 +0000

    multiple meshes now used for internal view

--- trunk/blend2csv.py
+++ trunk/blend2csv.py
@@ -1,122 +1,122 @@
 # Usage: ./blend data/models/cockpit.blend blend2csv.py 
 import bpy
 from mathutils import Color, Vector
+import os
+from os.path import join, splitext
+import sys
+
+blender_file=sys.argv[2]
+dest_dir=splitext(blender_file)[0]
+print('dest_dir: '+dest_dir)
+try:
+    for path in os.listdir(dest_dir):
+        os.remove(join(*(dest_dir,path)))
+    os.rmdir(dest_dir)
+except OSError:
+    pass
+os.mkdir(dest_dir)
 
 scene=bpy.context.scene
 obs=scene.objects
-num_obs=0
 for o in obs:
     try:
         if o.data.vertices and o.is_visible(scene):
-            num_obs+=1
+            out=open(join(*(dest_dir, o.name)), "w")
+        else:
+            continue
     except AttributeError:
-        pass
+        continue
 
-print('Objects, ', num_obs)
-print('Object:, First Vertice:, Vertices:, First Triangle:, Triangles:')
+    out.write('Objects, 1\n')
+    out.write('Object:, First Vertice:, Vertices:, First Triangle:, Triangles:\n')
 
-v_tot=0
-t_tot=0
-v_next=1
-t_next=1
-tri2vert={}
-for o in obs:
-    try:
-        if o.is_visible(scene):
-            m=o.data
-            print(m.name, ', ', v_next, ', ', len(m.vertices), ', ', t_next, ', ', len(m.faces)*2)
-            v_next+=len(m.vertices)
-            t_next+=len(m.faces)*2
-            for v in m.vertices:
-                v_tot+=1
+    v_tot=0
+    t_tot=0
+    v_next=1
+    t_next=1
+    tri2vert={}
 
-            for f in m.faces:
-                verts=[]
-                for v_ref in f.vertices:
-                    verts.append(v_ref)
-                try:
-                    t_tot+=1
-                    if len(verts)==4:
-                        t_tot+=1
-                    assert len(verts)<=4
-                except AssertionError:
-                    print('Expecting 4 verts while counting: ', verts, ' m: ', m, ' f: ', f)
-                    exit(-1)
-    except AttributeError:
-        pass
+    m=o.data
+    out.write(m.name + ', ' + str(v_next) + ', ' + str(len(m.vertices)) + ', ' + str(t_next) + ', ' + str(len(m.faces)*2)+'\n')
+    v_next+=len(m.vertices)
+    t_next+=len(m.faces)*2
+    for v in m.vertices:
+        v_tot+=1
 
-print()
-print('Vertices, ',v_tot)
-print('Vertice:, X:, Y:, Z:')
-v_num=0
-for o in obs:
-    try:
-        if o.is_visible(scene):
-            m=o.data
-            for v in m.vertices:
-                v_num+=1
-                tri2vert[m,v.index]=v_num
-                s=o.scale
-                l=o.location
-                r=o.rotation_euler.to_quaternion()
-                vsr=r*Vector((v.co.x*s.x, v.co.y*s.y, v.co.z*s.z))
-                print(v_num,', ', vsr.x+l.x, ', ', vsr.y+l.y, ', ', vsr.z+l.z)
-    except AttributeError:
-        pass
+    for f in m.faces:
+        verts=[]
+        for v_ref in f.vertices:
+            verts.append(v_ref)
+        try:
+            t_tot+=1
+            if len(verts)==4:
+                t_tot+=1
+            assert len(verts)<=4
+        except AssertionError:
+            print('Expecting 4 verts while counting: ', verts, ' m: ', m, ' f: ', f)
+            exit(-1)
 
-print()
-print('Triangles, ', t_tot)
-print('Triangle:, Side1:, Side2:, Side3:')
+    out.write('\n')
+    out.write('Vertices, '+str(v_tot)+'\n')
+    out.write('Vertice:, X:, Y:, Z:\n')
+    v_num=0
 
-t_num=0
-for o in obs:
-    try:
-        if o.is_visible(scene):
-            m=o.data
-            for f in m.faces:
-                verts=[]
-                for v_ref in f.vertices:
-                    verts.append(v_ref)
-                try:
-                    t_num+=1
-                    print(t_num, ', ', tri2vert[m,verts[0]], ', ', tri2vert[m,verts[1]], ', ', tri2vert[m,verts[2]])
-                    if len(verts)==4:
-                        t_num+=1
-                        print(t_num, ', ', tri2vert[m,verts[0]], ', ', tri2vert[m,verts[2]], ', ', tri2vert[m,verts[3]])
-                    assert len(verts)<=4
-                except AssertionError:
-                    print('Expecting 4 verts in triangles: ', verts, ' m: ', m, ' f: ', f)
-                    exit(-1)
-    except AttributeError:
-        pass
+    m=o.data
+    for v in m.vertices:
+        v_num+=1
+        tri2vert[m,v.index]=v_num
+        s=o.scale
+        l=o.location
+        r=o.rotation_euler.to_quaternion()
+        vsr=r*Vector((v.co.x*s.x, v.co.y*s.y, v.co.z*s.z))
+        out.write(str(v_num) +str(', ')+ str(vsr.x+l.x)+ ', '+ str(vsr.y+l.y)+ ', '+ str(vsr.z+l.z)+'\n')
 
-print()
-print('Colors, ', t_tot)
-print('Triangle:, Red:, Green:, Blue:')
+    out.write('\n')
+    out.write('Triangles, '+str(t_tot)+'\n')
+    out.write('Triangle:, Side1:, Side2:, Side3:\n')
 
-t_num=0
-BLACK=Color((0,0,0))
-for o in obs:
-    try:
-        if o.is_visible(scene):
-            m=o.data
-            if len(m.materials):
-                c=m.materials[0].diffuse_color
-            else:
-                c=BLACK
-            for f in m.faces:
-                verts=[]
-                for v_ref in f.vertices:
-                    verts.append(v_ref)
-                try:
-                    t_num+=1
-                    print(t_num, ', ', round(c.r*0xff), ', ', round(c.g*0xff), ', ', round(c.b*0xff))
-                    if len(verts)==4:
-                        t_num+=1
-                        print(t_num, ', ', round(c.r*0xff), ', ', round(c.g*0xff), ', ', round(c.b*0xff))
-                    assert len(verts)<=4
-                except AssertionError:
-                    print('Expecting 4 verts in colours: ', verts, ' m: ', m, ' f: ', f)
-                    exit(-1)
-    except AttributeError:
-        pass
+    t_num=0
+
+    m=o.data
+    for f in m.faces:
+        verts=[]
+        for v_ref in f.vertices:
+            verts.append(v_ref)
+        try:
+            t_num+=1
+            out.write(str(t_num)+', '+str(tri2vert[m,verts[0]])+', '+str(tri2vert[m,verts[1]])+', '+str(tri2vert[m,verts[2]])+'\n')
+            if len(verts)==4:
+                t_num+=1
+                out.write(str(t_num)+', '+str(tri2vert[m,verts[0]])+', '+str(tri2vert[m,verts[2]])+', '+str(tri2vert[m,verts[3]])+'\n')
+            assert len(verts)<=4
+        except AssertionError:
+            print('Expecting 4 verts in triangles: ', verts, ' m: ', m, ' f: ', f)
+            exit(-1)
+
+    out.write('\n')
+    out.write('Colors, '+str(t_tot)+'\n')
+    out.write('Triangle:, Red:, Green:, Blue:\n')
+
+    t_num=0
+    BLACK=Color((0,0,0))
+
+    m=o.data
+    if len(m.materials):
+        c=m.materials[0].diffuse_color
+    else:
+        c=BLACK
+    for f in m.faces:
+        verts=[]
+        for v_ref in f.vertices:
+            verts.append(v_ref)
+        try:
+            t_num+=1
+            out.write(str(t_num)+', '+str(round(c.r*0xff))+', '+str(round(c.g*0xff))+', '+str(round(c.b*0xff))+'\n')
+            if len(verts)==4:
+                t_num+=1
+                out.write(str(t_num)+', '+str(round(c.r*0xff))+', '+str(round(c.g*0xff))+', '+str(round(c.b*0xff))+'\n')
+            assert len(verts)<=4
+        except AssertionError:
+            print('Expecting 4 verts in colours: ', verts, ' m: ', m, ' f: ', f)
+            exit(-1)
+    out.close()
