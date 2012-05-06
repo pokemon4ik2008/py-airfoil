bd5007adaf51d44e6cf41f9367c2bd52cb47f918
K1eran.Wh17e@gmail.com <K1eran.Wh17e@gmail.com@60fda64a-5d68-a757-9b40-cfa5bbaf896f>
2012-05-06 23:54:37 +0000

    reenabled estimations

--- proxy.py
+++ proxy.py
@@ -204,15 +204,15 @@ class ControlledSer(Mirrorable):
         
         return Mirrorable.deserialise(self, ser, estimated).setPos(Vector3(px,py,pz)).setAttitude(Quaternion(aw,ax,ay,az)).setVelocity(Vector3(vx,vy,vz))
 
-    # def isClose(self, obj):
-    #     if (self.getPos()-obj.getPos()).magnitude_squared()>=0.22:
-    #         return False
-    #     perm_att=self.getAttitude()
-    #     temp_att=obj.getAttitude().conjugated()
-    #     diff=(perm_att*temp_att)
+    def isClose(self, obj):
+        if (self.getPos()-obj.getPos()).magnitude_squared()>=0.22:
+            return False
+        perm_att=self.getAttitude()
+        temp_att=obj.getAttitude().conjugated()
+        diff=(perm_att*temp_att)
         
-    #     #print 'isClose: '+str(Vector3(diff.x, diff.y, diff.z).magnitude_squared())
-    #     return Vector3(diff.x, diff.y, diff.z).magnitude_squared()<0.0001
+        #print 'isClose: '+str(Vector3(diff.x, diff.y, diff.z).magnitude_squared())
+        return Vector3(diff.x, diff.y, diff.z).magnitude_squared()<0.0001
 
 class SerialisableFact:
     __OBJ_IDX,__TIME_IDX=range(2)
