2dad2342accdb22e26c2cc70fe56b4de7e755e6e
K1eran.Wh17e@gmail.com <K1eran.Wh17e@gmail.com@60fda64a-5d68-a757-9b40-cfa5bbaf896f>
2012-05-06 21:51:05 +0000

    fixes linking problem where libGLU wasn't added as a cterrain dependency

--- trunk/install.py
+++ trunk/install.py
@@ -20,7 +20,7 @@ else:
 if os.name == 'nt':
         cmd = '"\"%VS80COMNTOOLS%..\IDE\devenv.exe\"" c\cterrain.vcproj /build debug /Out buildlog.txt'
 elif os.name == 'posix':
-	cmd = 'g++ c/cterrain.c c/collider.cpp -Ic -Ic/Eigen -I/usr/X11R6/include -I/usr/local/include -lGL -lGLU -lMini -o bin/cterrain.so -O3 -shared -fPIC'
+	cmd = 'g++ c/cterrain.c c/collider.cpp -Ic -Ic/Eigen -I/usr/X11R6/include -I/usr/local/include -lMini -lGL -lGLU -o bin/cterrain.so -O3 -shared -fPIC'
 
 print "Running build command : \n" + cmd
 retVal = os.system(cmd)
