ef0dfa925dfef8b81c40d537b64c0fab08d38e68
K1eran.Wh17e@gmail.com <K1eran.Wh17e@gmail.com@60fda64a-5d68-a757-9b40-cfa5bbaf896f>
2013-02-19 00:45:08 +0000

    work on headless server

---
+++ api.py
@@ -0,0 +1,42 @@
+from os import getcwd, remove
+
+PYGAME='pygame'
+PYGLET='pyglet'
+
+FILE='compile_type.txt'
+
+def setAPI(api):
+        print getcwd()
+        print 'setting api to '+api
+        try:
+                compileType=None
+                compileType=open(FILE, 'w')
+                compileType.write(api)
+                compileType.close()
+                print 'setting api success '+api
+        except IOError:
+                print 'failed to set compile type. attempting to default to pygame'
+        finally:
+                if compileType is not None:
+                        compileType.close()
+                remove(FILE)
+
+def setPyGame():
+        setAPI(PYGAME)
+
+def setPyglet():
+        setAPI(PYGLET)
+
+def getAPI():
+        api=PYGAME
+        try:
+                compileType=None
+                compileType=open(FILE, 'r')
+                api=compileType.read()
+        except IOError:
+                print 'failed to read compile type. assuming pygame'                
+        finally:
+                if compileType is not None:
+                        compileType.close()
+                        
+        return api
