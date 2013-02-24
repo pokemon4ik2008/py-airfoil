4f822d98789a0e04bda956d3d0e2e411847384cd
K1eran.Wh17e@gmail.com <K1eran.Wh17e@gmail.com@60fda64a-5d68-a757-9b40-cfa5bbaf896f>
2013-02-24 23:24:33 +0000

    switching camera subjects now no longer confuses the selected id of a particular view

--- subject.py
+++ subject.py
@@ -3,37 +3,31 @@ import manage as man
 class SubjectSelector:
         def __init__(self, proxy, types, views):
                 self.__proxy=proxy
-                self.__id=None
                 self.__types=types
                 for v in views:
                         v.push_handlers(self)
                 
         def on_subject_change(self, view, adjustment):
-                view.link(self.pick(adjustment))
+                view.link(self.pick(view, adjustment))
 
         def __get_subjects(self):
                 planes=[ plane for plane in man.proxy.getTypesObjs(self.__types) ]
                 ids=[ plane.getId() for plane in planes ]
                 return (ids, planes)
 
-        def pick(self, adj):
-                print 'SubjectSelector.pick'
+        def pick(self, view, adj):
                 (ids, subs)=self.__get_subjects()
                 try:
-                        print 'SubjectSelector.pic. subs: '+str(subs)+' id: '+str(self.__id)
-                        index=ids.index(self.__id)
+                        print 'pick. ids: '+str(ids)
+                        index=ids.index(view.getPlaneId())
                         index=(index+adj)%len(subs)
                         sub=subs[index]
-                        self.__id=ids[index]
-                        print 'SubjectSelector.pick: '+str(self.__id)
                         return sub
                 except ValueError:
                         if len(subs)>0:
                                 sub=subs[0]
-                                self.__id=ids[0]
-                                print 'SubjectSelector.exception: '+str(self.__id)
+                                print 'SubjectSelector.exception'
                                 return sub
                         else:
-                                self._id=None
                                 print 'SubjectSelector.exception. returning none '
                                 return None
